a9d09dc3b10ce6012bac0a940111b56f58d146c9
dani <maildedani9@gmail.com>
2020-06-21 02:19:58 +0200

    totalmente funcional con themes y comentarios js

--- functions.js
+++ functions.js
@@ -1,58 +1,58 @@
 console.log("JS is running");
 
-const display = document.getElementById('display');
+const display = document.getElementById('display');    //  establece la constante display como el elento input del DOM
 
-let valor1;
-let ClickNumero =  false;
-let resultado = null;
-let operador = null;
-display.value = "0";
-function push(id) {
-    if(ClickNumero == false || valor1 == "0" ) {limpiarDisplay()}
-    ClickNumero = true;
+let valor1;                 //  sera el valor que se acumula con los clicks
+let ClickNumero =  false;  // controlara si el ultimo boton clicado es numerico
+let resultado = null;   // el resultado acumulado en display
+let operador = null;    //  registra el signo clicado para la proxima accion
+display.value = "0";    //inicia a cero el valor de display
+function push(id) {    // funcion inserta valores clicados en display
+    if(ClickNumero == false || valor1 == "0" ) {limpiarDisplay()}  // evita secuencias de signos en el display
+    ClickNumero = true;  // controla si el ultimo boton pulsado fue un numero
     click(id); //ejecuta el click visual del boton
     display.value += id;    // añade numero pulsado al display
-    valor1 = (parseFloat(display.value));
+    valor1 = (parseFloat(display.value));  // pasa el valor del display a un numero real
 }  
-function calcular(id) {
+function calcular(id) {      // funcion que ejecutara toso el proceso del calculo
     
-    ClickNumero = false;
+    ClickNumero = false;     
     click(id);
-    display.value = valor1;
+    display.value = valor1;   
     
-    switch (operador) {
-        case "+" :
+    switch (operador) {      //  switch para cada operacion diferente 
+        case "+" :    // caso suma
             if(resultado == null){resultado = 0;}
             resultado = resultado + valor1;
         break;
-        case "-" :
+        case "-" :   //  caso resta
             if(resultado == null){resultado = 0;}
             resultado = resultado - valor1;
         break;
-        case "*" :
+        case "*" :    // caso multiplicacion
             if(resultado == null){resultado = 1;}
             resultado = resultado * valor1;
         break;
-        case "/" :
+        case "/" :     // caso division
             if(resultado == null){resultado = 1;}
             resultado = (resultado / valor1);
         break;
         
-        case null :
+        case null :    //  caso que no haya valor
             resultado = valor1;
         break;
 
     }
-    if (id == "=" || id == "Enter") {
-        display.value = resultado;
-        operador = null;
-        valor1 = null;
+    if (id == "=" || id == "Enter") {      //  tratamiento para el operador igual o tecla enter
+        display.value = resultado;  // muestra resultado en display
+        operador = null;    // reinicia el operador a null
+        valor1 = null;  // reinicia el valor inicial del display a null
     } else {
         operador = id;
     }
 }
 
-function click(id){
+function click(id){     //  funcion efecto visual al pulsar un boton
     if ( id == "Enter"){
         id = "=";
     }
@@ -62,17 +62,16 @@ function click(id){
     setTimeout(function (){boton.style.cssText = " transform: translate(0,0);transition: transition: all 0.08s linear"},80); 
 }
 
-function limpiarDisplay(){
+function limpiarDisplay(){    //  funcion para limpiar display
     display.value = " ";
 }
-document.addEventListener('keydown', (e) => { 
-    let key = e.key;
-    console.log(key);
+document.addEventListener('keydown', (e) => {   // identifica las entradas por teclado fisico
+    let key = e.key;       
     keyboard(key);
 
 });
 
-function keyboard(key){
+function keyboard(key){   //  funcion que lanzada el valor pasado por teclado fisico a las unciones anteriores
     
     if( key <= 9){
         push(key);
@@ -85,8 +84,3 @@ function keyboard(key){
     
     }
 }
-function myFunction(event) {
-    var x = event.key;
-    console.log("la tecla es la" + x)
-    }
-
---
+++ settings.js
@@ -0,0 +1,43 @@
+
+
+let link = document.getElementById('theme--css');
+let buttonDark = document.getElementById('btn-dark');
+let buttonLight = document.getElementById('btn-light');
+let buttonw95 = document.getElementById('btn-w95');
+buttonDark.disabled = true;
+
+
+function ligthTheme() {     // para cambiar a colores claros
+    click("btn-light");     // reutiliza la funcion click de settings.js para efecto visual del click
+
+       setTimeout (function () {
+           link.href  = "light.css";  // cambia el valor href del link de css
+           buttonLight.disabled = true; // desactiva el light theme
+           buttonDark.disabled = false; //activa el dark theme
+           buttonw95.disabled = false;  //desactiva el w95 theme
+             
+        },200);     // se aplica un retraso de ejecucion por un tema puramente estetico de efecto al pulsar boton
+    }
+
+
+function darkTheme() {  // funcion exacta a la anterior pero cambia a colores oscuros
+    click("btn-dark");
+
+    setTimeout  (function () {
+            link.href = "dark.css";
+            buttonDark.disabled = true;    // desactiva boton dark theme
+            buttonLight.disabled = false;   //  activa boton light theme
+            buttonw95.disabled = false;  //desactiva el w95 theme
+    },200);
+    }
+
+    function w95Theme() {  // funcion exacta a la anterior pero cambia a colores oscuros
+        click("btn-w95");
+    
+        setTimeout  (function () {
+                link.href = "w95.css";
+                buttonw95.disabled = true;  // activa el w95 theme
+                buttonDark.disabled = false;    // desactiva boton dark theme
+                buttonLight.disabled = false;   //  activa boton light theme
+        },200);
+        }
